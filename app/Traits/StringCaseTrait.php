8ae2794613ee048f12789297675ad3f33e31ca1a
Tafara Shamu <talph@outlook.com>
2022-07-19 16:19:35 +0200

    String case formatting trait

---
+++ app/Traits/StringCaseTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits;
+
+trait StringCaseTrait
+{
+
+    public function stringCapitalizeFirstLetter(?string $string): string
+    {
+        return ucfirst(strtolower($string));
+    }
+
+    public function stringLowerLetters(?string $string): string
+    {
+        return strtolower($string);
+    }
+
+    public function stringCapitalizeAllFirstLetters(?string $string): string
+    {
+        return ucwords($string);
+    }
+
+    public function stringCapitalizeAllLetters(?string $string): string
+    {
+        return strtoupper($string);
+    }
+}
